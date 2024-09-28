445565fa1cd81ad252528d9c1193634551093556
dattesuki <dattesuki@mail.ru>
2024-09-29 00:25:54 +0300

    tbitfield 0.1
    были реализованы некоторые методы класса tbitfield(до операции сравнения)

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -5,7 +5,14 @@
 //
 // Битовое поле
 
+/*int  BitLen; // длина битового поля - макс. к-во битов
+  TELEM *pMem; // память для представления битового поля
+  int  MemLen; // к-во эл-тов Мем для представления бит.поля*/
+
+
 #include "tbitfield.h"
+
+using namespace std;
 
 // Fake variables used as placeholders in tests
 static const int FAKE_INT = -1;
@@ -13,56 +20,101 @@ static TBitField FAKE_BITFIELD(1);
 
 TBitField::TBitField(int len)
 {
+    if (len < 0) throw std::underflow_error("underflow_error"); //обработка ошибки, если кол-во эл-ов не положительное
+    BitLen = len; //количество элементов
+    MemLen = (len / (sizeof(TELEM) * 8)) + 1;//к-во эл-тов Мем для представления бит.поля = количество элементов / количество элементов в TELEM
+    pMem = new TELEM[MemLen]{};
+    for (int i = 0; i < (MemLen); i++) {
+        pMem[i] = 0;
+    }
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
+        BitLen = bf.BitLen; //проверка на кол-во эл-ов уже выполнялась
+        MemLen = BitLen / (sizeof(TELEM) * 8);
+        pMem = new TELEM[MemLen];
+        for (int i = 0; i < (MemLen); i++) {
+            pMem[i] = bf.pMem[i];
+    }
 }
 
 TBitField::~TBitField()
 {
+    delete[] pMem;
 }
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
-    return FAKE_INT;
+    if (n < 0) throw std::underflow_error("underflow_error"); //обработка ошибки, если элемент отрицательный
+    if (n > BitLen) throw out_of_range("out_of_range"); //обработка ошибки, если происходит попытка доступа к элементу за границей заданного диапазона
+    int NumberMem = MemLen - (n / (sizeof(TELEM) * 8)) - 1;
+    int NumberElement = ((sizeof(TELEM) * 8) - 1) - (n % (sizeof(TELEM) * 8));
+    return NumberMem;
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
-    return FAKE_INT;
+    if (n < 0) throw std::underflow_error("underflow_error"); //обработка ошибки, если элемент отрицательный
+    if (n > BitLen) throw out_of_range("out_of_range"); //обработка ошибки, если происходит попытка доступа к элементу за границей заданного диапазона
+    int NumberMem = MemLen - (n / (sizeof(TELEM) * 8)) - 1;
+    return pMem[NumberMem];
 }
 
 // доступ к битам битового поля
 
 int TBitField::GetLength(void) const // получить длину (к-во битов)
 {
-  return FAKE_INT;
+  return BitLen;
 }
 
 void TBitField::SetBit(const int n) // установить бит
 {
+    if (n < 0) throw std::underflow_error("underflow_error"); //обработка ошибки, если элемент отрицательный
+    if (n > BitLen) throw out_of_range("out_of_range"); //обработка ошибки, если происходит попытка доступа к элементу за границей заданного диапазона
+    int NumberMem = MemLen - (n / (sizeof(TELEM) * 8)) - 1;
+    int NumberElement = ((sizeof(TELEM) * 8) - 1) - (n % (sizeof(TELEM) * 8));
+    pMem[NumberMem] |= (1 << NumberElement);
 }
 
 void TBitField::ClrBit(const int n) // очистить бит
 {
+    if (n < 0) throw std::underflow_error("underflow_error"); //обработка ошибки, если элемент отрицательный
+    if (n > BitLen) throw out_of_range("out_of_range"); //обработка ошибки, если происходит попытка доступа к элементу за границей заданного диапазона
+    int NumberMem = MemLen - (n / (sizeof(TELEM) * 8)) - 1;
+    int NumberElement = ((sizeof(TELEM) * 8) - 1) - (n % (sizeof(TELEM) * 8));
+    pMem[NumberMem] &= ~(1 << NumberElement);
 }
+
 
 int TBitField::GetBit(const int n) const // получить значение бита
 {
-  return FAKE_INT;
+    if (n < 0) throw std::underflow_error("underflow_error"); //обработка ошибки, если элемент отрицательный
+    if (n > BitLen) throw out_of_range("out_of_range"); //обработка ошибки, если происходит попытка доступа к элементу за границей заданного диапазона
+    int NumberMem = MemLen - (n / (sizeof(TELEM) * 8)) - 1;
+    int NumberElement = ((sizeof(TELEM) * 8) - 1) - (n % (sizeof(TELEM) * 8));
+    return (pMem[NumberMem]&(1 << NumberElement));
 }
 
 // битовые операции
 
 TBitField& TBitField::operator=(const TBitField &bf) // присваивание
 {
-    return FAKE_BITFIELD;
+    if (this != &bf) {
+        BitLen = bf.BitLen; //проверка на кол-во эл-ов уже выполнялась
+        MemLen = BitLen / (sizeof(TELEM) * 8);
+        pMem = new TELEM[MemLen];
+        for (int i = 0; i < (MemLen); i++) {
+            pMem[i] = bf.pMem[i];
+        }
+    }
+    return *this;
 }
 
 int TBitField::operator==(const TBitField &bf) const // сравнение
 {
-  return FAKE_INT;
+    
+  return 1;
 }
 
 int TBitField::operator!=(const TBitField &bf) const // сравнение
