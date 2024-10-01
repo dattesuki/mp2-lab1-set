cb8b00f22bdb8554a1bc69bdd640cf1562467cfa
dattesuki <dattesuki@mail.ru>
2024-10-02 00:32:03 +0300

    зануление указателя после освобождения памяти

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -39,6 +39,7 @@ TBitField::TBitField(const TBitField& bf) // конструктор копиро
 TBitField::~TBitField()
 {
     delete[] pMem;
+    pMem = nullptr;
 }
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
